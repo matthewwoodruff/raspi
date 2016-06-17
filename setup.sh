0038082b03d8ea285199256e7913d64c2a5acff5
Matt Woodruff <mwoodruf@thoughtworks.com>
2016-06-17 20:10:53 +0100

    Unmount disk after formatting in setup script

--- setup.sh
+++ setup.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 
-image = $1
+image=$1
 
 set -xe
 
@@ -10,5 +10,6 @@ read -p "Enter disk num to format: /dev/disk<num>: " diskNum
 
 diskutil unmountDisk /dev/disk${diskNum}
 diskutil eraseDisk FAT32 UNTITLED /dev/disk${diskNum}
+diskutil unmountDisk /dev/disk${diskNum}
 sudo dd bs=1m if=${image} of=/dev/rdisk${diskNum}
 
