697dbf44ddde0ee034ab4ee305e27b88ed72f9da
Matt Woodruff <mwoodruf@thoughtworks.com>
2016-10-23 15:24:45 +0100

    Updated docker install playbook

--- setup.sh
+++ setup.sh
@@ -11,5 +11,5 @@ read -p "Enter disk num to format: /dev/disk<num>: " diskNum
 diskutil unmountDisk /dev/disk${diskNum}
 diskutil eraseDisk FAT32 UNTITLED /dev/disk${diskNum}
 diskutil unmountDisk /dev/disk${diskNum}
-sudo dd bs=1m if=${image} of=/dev/rdisk${diskNum}
+sudo dd bs=1m if=${image} of=/dev/disk${diskNum}
 
